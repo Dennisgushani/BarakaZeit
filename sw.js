d91914f49b24256b47c03e423fd38ef3762a6993
Dennisgushani <frnndgushani@gmail.com>
2026-04-04 04:28:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,22 @@
-const cacheName = 'gebet-app-v2'; // Version auf v2 geändert
+const cacheName = 'gebet-app-v3';
 const assets = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(assets))
   );
 });
 
-// Verbesserte Logik: Erst Internet versuchen, dann Cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
+    caches.match(event.request).then(response => {
+      // Wenn im Cache, nimm es. Wenn nicht, hol es aus dem Internet und speichere eine Kopie!
+      return response || fetch(event.request).then(fetchRes => {
+        return caches.open(cacheName).then(cache => {
+          cache.put(event.request.url, fetchRes.clone());
+          return fetchRes;
+        });
+      });
     })
   );
 });
